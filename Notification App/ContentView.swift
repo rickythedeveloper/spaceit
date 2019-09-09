741c2465bdad4ca71dd172d1513df5e307876f7e
Ricky <rintaro.kawagishi@gmail.com>
2019-09-09 18:34:11 +0100

    Initial Commit

---
+++ Notification App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Notification App
+//
+//  Created by Rintaro Kawagishi on 09/09/2019.
+//  Copyright © 2019 Rintaro Kawagishi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
