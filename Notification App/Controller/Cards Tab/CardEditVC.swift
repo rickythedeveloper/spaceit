ff33d81d5eb61ff471070b44fa03f343e3b03811
Ricky <rintaro.kawagishi@gmail.com>
2020-01-04 21:37:49 +0900

    Now we can begin on the card edit vc

---
+++ Notification App/Controller/Cards Tab/CardEditVC.swift
@@ -0,0 +1,18 @@
+//
+//  CardEditVC.swift
+//  Notification App
+//
+//  Created by Rintaro Kawagishi on 04/01/2020.
+//  Copyright © 2020 Rintaro Kawagishi. All rights reserved.
+//
+
+import UIKit
+
+class CardEditVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
